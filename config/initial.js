0bee406cb758c2a05c1a9917b2d5a061da716d1a
theGuy1024 <72155917+theGuy1024@users.noreply.github.com>
2020-12-09 03:56:29 -0600

    Update initial.js

--- config/initial.js
+++ config/initial.js
@@ -1,17 +1,1 @@
-localStorage.setItem('language', 'english')
 
-function checkCookie(){
-    var lang = localStorage.getItem('language')
-    
-    if(lang === 'english'){
-        alert(lang)
-    }
-}
-
-function showdis(){
-    document.getElementById('target').classList.add('show');
-}
-
-function hidedis(){
-    document.getElementById('target').classList.add('hide');
-}
